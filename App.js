613db5bf61191352c85bf43ff603d081a5b3bb10
Afonso Henrique <hafonso021@gmail.com>
2022-06-29 08:28:30 -0300

    feat(AppJS): Update

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import {Text, View, TextInput} from 'react-native';
+import { Button } from 'react-native-web';
+import { useState } from 'react/cjs/react.production.min';
 
 export default () => {
+  const [numero, setNumero] = useState(0);
+  const [dobro, setDobro] = useState(0);
+
+  function calcularDobro(){
+    setDobro(parseInt(numero)*2)
+  }
+  return (
   <View>
     <Text>Cálculo do Dobro</Text>
+
+    <TextInput placeholder='Digite um número' onChangeText={setNumero}/>
+
+    <Button title="Calcular dobro" onPress={calcularDobro}/>
+
+    <Text>{dobro}</Text>
   </View>
+  )
 }
